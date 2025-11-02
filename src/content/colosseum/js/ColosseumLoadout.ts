4556dbbf2491c8dd54b7bf5876732232111d8089
vaisest <4550061+vaisest@users.noreply.github.com>
2025-11-02 19:11:31 +0200

    add noxious halberd to inventory

--- src/content/colosseum/js/ColosseumLoadout.ts
+++ src/content/colosseum/js/ColosseumLoadout.ts
@@ -1,6 +1,6 @@
 import { TorvaFullhelm, AmuletOfTorture, InfernalCape, DragonArrows, TorvaPlatebody, TorvaPlatelegs, PrimordialBoots, FerociousGloves, UltorRing, AvernicDefender, SuperCombatPotion, UnitOptions } from "osrs-sdk";
 import { SaradominBrew, SuperRestore } from "osrs-sdk";
-import { ScytheOfVitur, BladeOfSaeldor, Player } from "osrs-sdk";
+import { ScytheOfVitur, BladeOfSaeldor, NoxiousHalberd, Player } from "osrs-sdk";
 
 export class ColosseumLoadout {
   loadoutType: string;
@@ -27,7 +27,7 @@ export class ColosseumLoadout {
       inventory: [
         new BladeOfSaeldor(),
         new AvernicDefender(),
-        null,
+        new NoxiousHalberd(),
         null,
         new SaradominBrew(),
         new SaradominBrew(),
